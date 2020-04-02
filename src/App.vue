550d1d31fe4b8af28b1601b5b966f9df2f11bfdb
amadrocky <ak45amad@hotmail.fr>
2020-04-02 20:18:56 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Home from "./components/Home";
+
+export default {
+  name: "App",
+  components: {
+    Home
+  }
+};
+</script>
+
+<style>
+#app {
+  background-image: url("/assets/astronomy-1867616_1920.jpg");
+  background-size: cover;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <b-navbar type="light" variant="dark">
+      <b-nav-form @submit.prevent="search()">
+        <b-form-input class="mr-sm-2" placeholder="Search" v-model="movieSearch"></b-form-input>
+        <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
+      </b-nav-form>
+    </b-navbar>
+    <b-container>
+      <div id="info" v-if="infos !== null">
+        <b-row v-if="infos.Title" class="pt-5">
+          <b-col>
+            <p>TITLE: {{ infos.Title }}</p>
+            <br />
+            <p>YEAR: {{ infos.Year }}</p>
+            <br />
+            <p>RUNTIME: {{ infos.Runtime }}</p>
+            <br />
+            <p>GENRE: {{ infos.Genre }}</p>
+            <br />
+            <p>DIRECTOR: {{ infos.Director }}</p>
+            <br />
+            <p>ACTORS: {{ infos.Actors }}</p>
+            <br />
+            <p>PLOT: {{ infos.Plot }}</p>
+            <br />
+            <p>COUNTRY: {{ infos.Country }}</p>
+          </b-col>
+          <b-col class="text-center">
+            <b-img class="image" v-bind:src="infos.Poster" alt="cover"></b-img>
+          </b-col>
+        </b-row>
+        <div v-else>
+          <b-alert variant="warning" class="text-center mt-5" show>{{ infos.Error }}</b-alert>
+        </div>
+      </div>
+      <b-alert
+          v-else
+          variant="primary"
+          class="text-center mt-5"
+          show
+        >Veuillez executer une recherche</b-alert>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+Vue.use(VueAxios, axios);
+
+export default {
+  name: "Home",
+  data: () => ({
+    infos: null,
+    movieSearch: ""
+  }),
+  methods: {
+    search() {
+      axios
+        .get(`http://www.omdbapi.com/?t=${this.movieSearch}&apikey=8548d355`)
+        .then(response => (this.infos = response.data))
+        .catch(e => {
+          console.log("error", e);
+        });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
